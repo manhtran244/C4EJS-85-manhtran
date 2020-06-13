8961f047f68f3d11aa8c17142094c5ba953c6177
manhtran244 <66857598+manhtran244@users.noreply.github.com>
2020-06-13 12:09:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+// ex3
+// var string = "Coding is great"
+// console.log(string)
+
+// var number = studentCount = 0
+// console.log(number)
+
+// prompt('you look beautiful today')
+
+//ex4
+// var string1 = 'Coding might not be easy, but still great'
+// var z = string1.toLocaleLowerCase()
+// console.log(z)
+
+// var number1 = studentCount = 16
+// console.log(number1)
+
+//ex5
+// alert("you look so beautiful")
+
+//ex6
+// var name = prompt('Hi there, your name please')
+// alert('Hello ' + name)
+
+//ex7
+// var firstName = prompt('Enter your first name, please')
+// var middleName = prompt('Enter your middle name, please')
+// var lastName = prompt('Enter your last name, please ')
+
+// alert('Hello '+' '+lastName +' '+middleName+ ' '+firstName)
+
+//ex8
+// var square = prompt('Enter the side lenght of the square')
+
+// var result = square*square
+
+// alert('The square area is'+' ' +result)
+
+//ex9
+// var radius = prompt('Enter the radius of the circle')
+
+// var result = radius*radius*Math.PI
+
+// alert('The circle area is'+' '+ result)
+
+//ex10
+// var celsius = prompt('Enter the temperature in Celsius')
+
+// var result = celsius*(9/5)+32
+
+// alert(celsius+'(C)' + ' = '+ result +'(F)')
